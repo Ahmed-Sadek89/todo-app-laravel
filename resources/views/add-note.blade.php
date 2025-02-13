62b1754b01096e2703834eac4cf1b03465ccb15d
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2025-02-13 17:22:23 +0200

    complete the frontend

--- resources/views/add-note.blade.php
+++ resources/views/add-note.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.protected')
 @section('protected-title', 'Todo App | Add a new note')
 @section('protected-layout')
-    <h2 class="text-2xl">this is add note page</h2>
+    <x-add-todo-form />
 @endsection
